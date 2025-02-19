692d9ded2bd9dc84d08b455652fe50c149aef795
Ben Watt <77812385+continualWolf@users.noreply.github.com>
2025-02-19 10:12:28 +0000

    Sessions Page
    - Started on adding in the UI for the sessions page but still is a work in progress.

--- wwwroot/javascript/main.js
+++ wwwroot/javascript/main.js
@@ -7,7 +7,41 @@ $(document).ready(function(){
         $('.main-content').hide();
         $('#profilePage').show();
     });
+
+    $('.joinSessionButton').on('click', function(){
+       $('#sessionPage').append(joinSessionConfirmationPopUp()); 
+       $('.clockIcon').attr('src', 'wwwroot/icons/clock-regular.svg');
+       attachClosePopUpEvent()
+    });
+
+   
 });
+
+function attachClosePopUpEvent(){
+    $('.close-pop-up').off('click').on('click', function(){
+        $(this).closest('.pop-up-overlay').remove();
+    });
+}
+
+const joinSessionConfirmationPopUp = () => {
+    return `
+        <div class="pop-up-overlay">
+            <div class="pop-up">
+                <div class="pop-up-controls">
+                    <div class="close-pop-up">X</div>
+                </div>
+                <div class="pop-up-body">
+                    <h2 style="text-align: center; font-weight: 500;">Do you want to join the session now?</h2>
+                    <p style="display: flex; flex-direction: row; justify-content: center; align-items: center; gap:10px" > <img class="clockIcon" />1 min more  </p>
+                    <div style="display:flex;flex-flow:column;gap:25px;width: 100%; justify-content: center; align-items: center; margin-top: 40px;">
+                        <button class="block-btn w-150">Join Now</button>
+                        <button class="update-profile-btn w-150">Cancel</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
 
 function toggleNotifications() {
     const panel = document.getElementById('notificationPanel');
