70163dd42fdc0bd201f776f374e27a93ae036311
Ben Watt <77812385+continualWolf@users.noreply.github.com>
2025-03-08 02:46:13 +0000

    Start to mentor web portal pages
    I made a start to the mentor web portal pages mainly the first page, i also added in a calendar which is currently only visible on the mentee pages.

--- wwwroot/javascript/main.js
+++ wwwroot/javascript/main.js
@@ -127,6 +127,11 @@ function toggleNotifications() {
     panel.classList.toggle('active');
 }
 
+function toggleCalendar() {
+    const panel = document.getElementById('calendarPanel');
+    panel.classList.toggle('active');
+}
+
 function openBlankPage() {
     window.open('about:blank', '_self');
 }
@@ -150,4 +155,44 @@ function handlePageLinkClick(button, page) {
   
     $('#' + page).show();
   }
+  
+var app = {
+    settings: {
+      container: $('.calendar'),
+      calendar: $('.front'),
+      days: $('.weeks span'),
+      form: $('.back'),
+      input: $('.back input'),
+      buttons: $('.back button')
+    },
+  
+    init: function() {
+      instance = this;
+      settings = this.settings;
+      this.bindUIActions();
+    },
+  
+    swap: function(currentSide, desiredSide) {
+      settings.container.toggleClass('flip');
+  
+      currentSide.fadeOut(900);
+      currentSide.hide();
+      desiredSide.show();
+  
+    },
+  
+    bindUIActions: function() {
+      settings.days.on('click', function(){
+        instance.swap(settings.calendar, settings.form);
+        settings.input.focus();
+      });
+  
+      settings.buttons.on('click', function(){
+        instance.swap(settings.form, settings.calendar);
+      });
+    }
+  }
+  
+  app.init();
+  
   
